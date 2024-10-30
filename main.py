8e8e00555125a37450b31fa514fbef64a460444c
CarlosDanielArauzSanjuan~ <186082333+CarlosDanielArauzSanjuan@users.noreply.github.com>
2024-10-30 10:39:32 -0500

    Exercise9 "Final Score"

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-#Parte decimal
-#Escriba un programa que entregue la parte decimal de un número real ingresado por el usuario.
+#Un alumno desea saber que nota necesita en el tercer certamen para aprobar un ramo. El promedio del ramo se calcula con la siguiente formula.
 
-#Ingrese un numero: 4.5
-#0.5
-#Ingrese un numero: -1.19
-#0.19
+#Escriba un programa que pregunte al usuario las notas de los dos primeros certamen y la nota de laboratorio, 
+# y muestre la nota que necesita el alumno para aprobar el ramo con nota final 60.
 
-def decimal(number): #followed by the function name and parentheses containing any parameters
-    if number < 0: #cicle
-        return abs(number) - int(abs(number)) #abs means absolute value
-    return number - int(number)
+#Ingrese nota certamen 1: 45
+#Ingrese nota certamen 2: 55
+#Ingrese nota laboratorio: 65
+#Necesita nota 72 en el certamen 3
 
-numbers = float(input( "Enter the number: "))
-decimals =  decimal(numbers)
+def scoreNece(c1, c2, lab): #function calculates a score based on some criteria, such as points
+    nfinal = 60
+    NC = (c1 + c2 + 0) / 3  
+    C3 = ((nfinal- lab * 0.3) / 0.7) * 3 - c1 - c2
+    return C3
 
-print(f"""
-    The decimal value of the number is: {decimals}
-""")
+sc1 = float(input("Enter score 1: "))
+sc2 = float(input("Enter score 2: "))
+sclab = float(input("Enter score LAB: "))
+
+scorefinal = scoreNece(sc1, sc2, sclab)
+
+print(f"You need {scorefinal:.2f} score on the final exam ")
