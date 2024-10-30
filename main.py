1ad197c69ebc066e27d231db78d426eefce16858
CarlosDanielArauzSanjuan~ <186082333+CarlosDanielArauzSanjuan@users.noreply.github.com>
2024-10-30 11:35:04 -0500

    Exercise10 "cooking eggs"

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-#Un alumno desea saber que nota necesita en el tercer certamen para aprobar un ramo. El promedio del ramo se calcula con la siguiente formula.
+import math
+to = float(input("what is the temperature of the Egg\n")) 
+tw = 100
+ty = 70
+m = 47
+p = 1.038
+c = 3.7
+k = 5.4 * math.pow(10,-3)
 
-#Escriba un programa que pregunte al usuario las notas de los dos primeros certamen y la nota de laboratorio, 
-# y muestre la nota que necesita el alumno para aprobar el ramo con nota final 60.
+div = math.pow(m,(2/3)) * (c * (math.pow(p, (1/3))))
+div2 = (k * math.pow(math.pi, 2))* math.pow((4*math.pi) / 3, (2/3))
+total = div/div2 
+total2 = math.log(0.76 * ((to - tw) / (ty - tw)))
+sec = total * total2
+min = round(sec/60)
 
-#Ingrese nota certamen 1: 45
-#Ingrese nota certamen 2: 55
-#Ingrese nota laboratorio: 65
-#Necesita nota 72 en el certamen 3
-
-def scoreNece(c1, c2, lab): #function calculates a score based on some criteria, such as points
-    nfinal = 60
-    NC = (c1 + c2 + 0) / 3  
-    C3 = ((nfinal- lab * 0.3) / 0.7) * 3 - c1 - c2
-    return C3
-
-sc1 = float(input("Enter score 1: "))
-sc2 = float(input("Enter score 2: "))
-sclab = float(input("Enter score LAB: "))
-
-scorefinal = scoreNece(sc1, sc2, sclab)
-
-print(f"You need {scorefinal:.2f} score on the final exam ")
+print(f"""
+      The time to cook the egg is {round(sec)} seconds
+      the time to cook the egg is {min} minutes
+""")
